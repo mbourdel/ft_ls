b0f67929c343908af48fde737cfb1d329d27c6de
mbourdel <mlebourdelles@free.fr>
2015-02-11 16:25:59 +0100

    right find and apply on -l option

--- ft_option_l.c
+++ ft_option_l.c
@@ -18,22 +18,25 @@ static char		*ft_type(t_stat stat)
 
 	res = ft_memalloc(11);
 	S_ISDIR(stat.st_mode) ? (res[0] = 'd') : (res[0] = '-');
-	res[1] = '-';
-	res[2] = '-';
-	res[3] = '-';
-	res[4] = '-';
-	res[5] = '-';
-	res[6] = '-';
-	res[7] = '-';
-	res[8] = '-';
-	res[9] = '-';
-	res[10] = '\0';
+	(stat.st_mode & S_IRUSR) ? (res[1] = 'r') : (res[1] = '-');
+	(stat.st_mode & S_IWUSR) ? (res[2] = 'w') : (res[2] = '-');
+	(stat.st_mode & S_IXUSR) ? (res[3] = 'x') : (res[3] = '-');
+	(stat.st_mode & S_IRGRP) ? (res[4] = 'r') : (res[4] = '-');
+	(stat.st_mode & S_IWGRP) ? (res[5] = 'w') : (res[5] = '-');
+	(stat.st_mode & S_IXGRP) ? (res[6] = 'x') : (res[6] = '-');
+	(stat.st_mode & S_IROTH) ? (res[7] = 'r') : (res[7] = '-');
+	(stat.st_mode & S_IWOTH) ? (res[8] = 'w') : (res[8] = '-');
+	(stat.st_mode & S_IXOTH) ? (res[9] = 'x') : (res[9] = '-');
 	return (res);
 }
 
 void			ft_option_l(t_env *env)
 {
 	char	*yolo;
+
+	ft_putendl(ft_itoa(S_IRUSR));
+	ft_putendl(ft_itoa(S_IWUSR));
+	ft_putendl(ft_itoa(S_IXUSR));
 	while (env->file != NULL)
 	{
 		yolo = ft_type(env->file->stat);
