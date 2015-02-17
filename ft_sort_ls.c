97dbb084fc17f88646327980a6ee2be90950bde8
mbourdel <mlebourdelles@free.fr>
2015-02-17 13:35:35 +0100

    sort work yeah

--- ft_sort_ls.c
+++ ft_sort_ls.c
@@ -6,7 +6,7 @@
 /*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/17 12:01:20 by mbourdel          #+#    #+#             */
-/*   Updated: 2015/02/17 13:04:28 by mbourdel         ###   ########.fr       */
+/*   Updated: 2015/02/17 13:33:43 by mbourdel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static void	ft_switch_file(t_env *env)
 {
-	t_file		**tmp1;
-	t_file		**tmp2;
+	t_dirent		*tmp_d;
+	t_stat			tmp_s;
 
-	ft_putendl("before swap");
+/*	ft_putendl("\nbefore swap");
 	if (env->file->pvs)
 	{
 		ft_putstr(env->file->pvs->dirent->d_name);
@@ -32,7 +32,14 @@ static void	ft_switch_file(t_env *env)
 		ft_putstr(env->file->nxt->nxt->dirent->d_name);
 	}
 	ft_putendl("\nafter swap");
-
+*/
+	tmp_d = env->file->dirent;
+	tmp_s = env->file->stat;
+	env->file->dirent = env->file->nxt->dirent;
+	env->file->stat = env->file->nxt->stat;
+	env->file->nxt->dirent = tmp_d;
+	env->file->nxt->stat = tmp_s;
+	/*
 	tmp1 = &env->file;
 	tmp2 = &env->file->nxt;
 	if (env->file->pvs)
@@ -44,7 +51,8 @@ static void	ft_switch_file(t_env *env)
 	env->file->nxt->nxt = env->file;
 	env->file->pvs = env->file->nxt;
 	env->file->nxt = *tmp1;
-	if (env->file->pvs)
+	*/
+/*	if (env->file->pvs)
 	{
 		ft_putstr(env->file->pvs->dirent->d_name);
 		ft_putstr("\t--->\t");
@@ -57,7 +65,7 @@ static void	ft_switch_file(t_env *env)
 		ft_putstr("\t--->\t");
 		ft_putstr(env->file->nxt->nxt->dirent->d_name);
 	}
-
+*/
 	return ;
 }
 
